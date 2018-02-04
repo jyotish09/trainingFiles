707c554c89cc6cf90bc9f058dd4a4bf925941d8a
jypaul <dontsentmemails@yahoo.com>
2018-02-04 11:11:02 +0530

    Readability + Debugging

--- html/todoList/app.js
+++ html/todoList/app.js
@@ -1,19 +1,25 @@
 // Better One https://codepad.co/snippet/EH5sgBJT
 
 
-function get_todos() {
+function get_current_todos() {
+    console.groupCollapsed('get_current_todos()');
     var todos = new Array;
     var todos_str = localStorage.getItem('todo');
+    console.log(todos_str);
     if (todos_str !== null) {
         todos = JSON.parse(todos_str);
     }
+    console.log(todos);
+    console.groupEnd();
     return todos;
 }
 
 function add() {
     var task = document.getElementById('task').value;
-
-    var todos = get_todos();
+    console.groupCollapsed('add() task');
+    console.log(task);
+    console.groupEnd();
+    var todos = get_current_todos();
     todos.push(task);
     localStorage.setItem('todo', JSON.stringify(todos));
 
@@ -23,22 +29,26 @@ function add() {
 }
 
 function remove() {
+    console.groupCollapsed('remove() taskId');
     var id = this.getAttribute('id');
-    var todos = get_todos();
+    console.log(id);
+    var todos = get_current_todos();
+    console.log('todo');
+    console.log(todos);
     todos.splice(id, 1);
+    console.log(todos);
     localStorage.setItem('todo', JSON.stringify(todos));
-
+    console.groupEnd();
     show();
-
     return false;
 }
 
 function show() {
-    var todos = get_todos();
+    var todos = get_current_todos();
 
     var html = '<ul>';
     for(var i=0; i<todos.length; i++) {
-        html += '<li>' + todos[i] + '<button class="remove" id="' + i  + '">x</button></li>';
+        html += '<li>' + todos[i] + '<button class="remove" id="' + i  + '">X</button></li>';
     };
     html += '</ul>';
 
