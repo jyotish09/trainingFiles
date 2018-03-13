d2aa276b9d451001260c8584a8518b2f82f81f89
jypaul <dontsentmemails@yahoo.com>
2018-03-14 05:02:56 +0530

    Extra Examples

--- js/asyncStuff/app.js
+++ js/asyncStuff/app.js
@@ -1,7 +1,20 @@
 const url = 'https://randomuser.me/api/?results=10';
 fetch(url).then((resp) => resp.json()).then(function(data) {
-  console.log(data.results);
+  // console.log(data.results);
+  callback(data);
 })
 .catch(function(error) {
 console.log(error);
 });
+
+function callback(data){
+    if(!data){
+        console.log("NO data");
+    }else{
+        console.log("callback fn() data : ")
+        console.log(data)
+    }
+
+}
+
+callback()
